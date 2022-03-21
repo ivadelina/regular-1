35fdc253b0c4699fd86370b756a96f124f930a9f
ivadelina <kapranova.milla@yandex.ru>
2022-03-21 21:44:26 +0300

    main

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 export default class Validator {
   validateUsername(name) {
-    if (/^[a-z][\w_-]*[a-z]$/gmi.test(name) && !/\d\d\d/.test(name)) {
+    if (/^[a-z][\w_-]*[a-z]$/gmi.test(name) && !/\d{3}/.test(name)) {
       return true
     } throw new Error('Некорректное имя');
   }
